149e07beaa252be419f63cc01e7ff58244922f43
nidhisargar <41470371+nidhisargar@users.noreply.github.com>
2021-03-06 12:50:35 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,22 @@
+# Program to display the Fibonacci series up to n-th term
+
+num = int(input("How many values you want? "))
+
+n1, n2 = 0, 1
+
+count = 0
+
+if num <= 0:
+   print("Please enter a positive integer")
+elif num == 1:
+   print("Fibonacci sequence upto",num,":")
+   print(n1)
+else:
+   print("Fibonacci sequence:")
+   while count < num:
+       print(n1)
+       nth = n1 + n2
+       
+       n1 = n2
+       n2 = nth
+       count += 1
